8e69c40998a1eb2286c1e766211d0e3d506f2882
Lrodriguez72 <LoreR1972@gmail.com>
2023-04-06 23:16:41 -0300

    app, dashboard, card, icons, buttons

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { CardsModule } from '../pages/cards/cards.module';
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    CardsModule
+  ],
+
+  exports:[
+    DashboardComponent
+  ]
+
+})
+export class DashboardModule { }
