38a9f0a4bee5bd5b99c4cfd2117fec4ea54034a3
Lrodriguez72 <LoreR1972@gmail.com>
2023-04-19 09:03:10 -0300

    hacer que reconozca el async

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import { enviroment } from 'src/environments/environments';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
   showFiller = false;
